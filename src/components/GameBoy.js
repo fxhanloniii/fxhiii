7b806d71ac64b08c4026ecf085ad08fc4a098503
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-29 14:14:10 -0700

    updating about me page

--- src/components/GameBoy.js
+++ src/components/GameBoy.js
@@ -7,6 +7,7 @@ const GameBoy = ({ currentImage, handleNextImage, handlePrevImage }) => {
         <div className="screenBorder">
             <div className="screen">
                 {/* Image Slide Show When User Clicks A or B Button */}
+                <img src={currentImage} alt="slideshow"/>
             </div>
         </div>
         <div className="buttons">
@@ -19,10 +20,10 @@ const GameBoy = ({ currentImage, handleNextImage, handlePrevImage }) => {
                 </div>
             <div className="ab">
                 <div className="bButton">
-                    <button className="buttonB">B</button>
+                    <button className="buttonB" onClick={handlePrevImage}>B</button>
                 </div>
                 <div className="aButton">
-                    <button className="buttonA">A</button>
+                    <button className="buttonA"onClick={handleNextImage}>A</button>
                 </div>
             </div>
         </div>
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,10 +2,35 @@ import React, { useState } from 'react'
 import GameBoy from '../components/GameBoy'
 
 const AboutMe = () => {
-
+  const text1 = {
+    title: 'Frankie Hanlon',
+    description: 'Software Engineer\n Located In Los Angeles, CA',
+    additionalInfo: (
+      <>
+      <h3 className="summary">A Full-Stack Developer with a creative background who brings a curious and diligent mindset, always striving for ongoing growth and development.</h3>
+      <div className="resumeDiv">
+        <a href="assets/resume.pdf" target="_blank" rel="noopener noreferrer">View Resume</a>
+      </div>
+      <div className="icons">
+        <img src='assets/skillIcons/react.png' alt="icon"/>
+        <img src='assets/skillIcons/java-script.png' alt="icon"/>
+        <img src='assets/skillIcons/python.png' alt="icon"/>
+        <img src='assets/skillIcons/database-storage.png' alt="icon"/>
+      </div>
+      <div className="icons">
+        <img src='assets/skillIcons/css-3.png' alt="icon"/>
+        <img src='assets/skillIcons/django.png' alt="icon"/>
+        <img src='assets/skillIcons/docker.png' alt="icon"/>
+        <img src='assets/skillIcons/nodejs.png' alt="icon"/>
+      </div>
+      </>
+    ),
+  }
   const images = [
-    {src:'headshot.jpeg', text: 'Head Shot Image'},
-    {src:'mammoth.jpg', text: 'Mammoth Sking Image'},
+    {src:'./assets/aboutMeImages/headshot.jpeg', text: text1},
+    {src:'./assets/aboutMeImages/action.jpg', text: 'Experience'},
+    {src:'./assets/aboutMeImages/mammoth.jpg', text: 'Mammoth Sking Image'},
+    {src:'./assets/aboutMeImages/mammoth.jpg', text: 'Mammoth Sking Image'},
   ]
 
   const [currentImage, setCurrentImage] = useState(0);
@@ -23,7 +48,11 @@ const AboutMe = () => {
       <h1 className="aboutMeH1">About Me</h1>
       <div className="aboutMeContainer">
         <div className="aboutMeText">
-          <p>{images[currentImage].text}</p>
+          <p className="textTitle">{images[currentImage].text.title}</p>
+          <p className="textDescription" style={{ whiteSpace: 'pre-line' }}>{images[currentImage].text.description}</p>
+          <div>
+            {images[currentImage].text.additionalInfo}
+          </div>
         </div>
         <div className="gameBoyContainer">
           <GameBoy currentImage={images[currentImage].src} handleNextImage={handleNextImage} handlePrevImage={handlePrevImage} />
