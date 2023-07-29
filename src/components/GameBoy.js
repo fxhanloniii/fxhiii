2d10cc533bfccd828d1355531c6f106c9cac36aa
fxhanloniii <hanlonfrankiex@gmail.com>
2023-07-29 11:31:32 -0700

    added gameboy lookinng feature to my about me

---
+++ src/components/GameBoy.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const GameBoy = () => {
+  return (
+    <div className="gameBoy">
+        <div className="screenBorder">
+            <div className="screen">
+                
+            </div>
+        </div>
+        <div className="buttons">
+                <div className="dpad">
+                    <div className="dpad-up"><button className="up"></button></div>
+                    <div className="dpad-down"><button className="down"></button></div>
+                    <div className="dpad-left"><button className="left"></button></div>
+                    <div className="dpad-right"><button className="right"></button></div>
+                    <div className="dpad-middle"></div>
+                </div>
+            <div className="ab">
+                <div className="bButton">
+                    <button className="buttonB">B</button>
+                </div>
+                <div className="aButton">
+                    <button className="buttonA">A</button>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default GameBoy
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import GameBoy from '../components/GameBoy'
 
 const AboutMe = () => {
   return (
-    <div>
-        
+    <div className="aboutMe">
+      <h1 className="aboutMeH1">About Me</h1>
+      <div ckassName="aboutMeContainer">
+        <div className="aboutMeText">
+
+        </div>
+        <div className="gameBoyContainer">
+          <GameBoy />
+        </div>
+      </div>
     </div>
   )
 }
